b3ab4b6741d37f3446ba4eb474499eb53430889b
Kristopher <nhj940913@gmail.com>
2025-02-17 05:10:06 +0000

    [LeetCode Sync] Runtime - 2023 ms (5.02%), Memory - 51.4 MB (5.25%)

---
+++ my-folder/1160-letter-tile-possibilities/solution.java
@@ -0,0 +1,29 @@
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+
+    private Set<String> allSet = new HashSet<>();
+
+    public void numTilePossibilities(String tiles, Set<Integer> visited, String currStr) {
+        if (currStr.length() != 0) this.allSet.add(currStr);
+        char[] chars = tiles.toCharArray();
+        for(int i = 0; i < chars.length; i++) {
+            if (!visited.contains(i)) {
+                // Set<Integer> s = new HashSet<Integer>(visited);
+                // s.add(i);
+                visited.add(i);
+                this.numTilePossibilities(tiles, visited, currStr);
+                this.numTilePossibilities(tiles, visited, currStr + chars[i]);
+                visited.remove(i);
+            }
+        }
+    }
+
+    public int numTilePossibilities(String tiles) {
+        this.numTilePossibilities(tiles, new HashSet<>(), "");
+        System.out.println(allSet);
+        return allSet.size();
+    }
+}
