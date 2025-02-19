922f0ce113ead7f9e69b02758bd7358e25e4d767
Kristopher <nhj940913@gmail.com>
2025-02-19 01:12:56 +0000

    [LeetCode Sync] Runtime - 7 ms (13.52%), Memory - 42.9 MB (8.14%)

---
+++ my-folder/2456-construct-smallest-number-from-di-string/solution.java
@@ -0,0 +1,48 @@
+class Solution {
+
+    private Set<Integer> used = new HashSet<>();
+    private String ans = "";
+
+    private boolean smallestNumber(String pattern, int index) {
+        // System.out.println(ans);
+        if (index == pattern.length() + 1) {
+            return true;
+        }
+
+        for(int num = 1; num <= 9; num++) {
+            if (used.contains(num)) {
+                continue;
+            }
+
+            if (index > 0 &&
+                pattern.charAt(index - 1) == 'I' && 
+                Integer.parseInt(ans.charAt(ans.length()-1) + "") > num
+            ) {
+                continue;
+            }
+            
+            if (index > 0 &&
+                pattern.charAt(index - 1) == 'D' && 
+                Integer.parseInt(ans.charAt(ans.length()-1) + "") < num
+            ) {
+                continue;
+            }
+
+            used.add(num);
+            ans += num;
+            if(this.smallestNumber(pattern, index+1)) {
+                return true;
+            }
+            used.remove(num);
+            ans = ans.substring(0, ans.length()-1);
+        }
+        return false;
+    }
+
+    public String smallestNumber(String pattern) {
+        if (this.smallestNumber(pattern, 0)) {
+            return this.ans;
+        }
+        return this.ans;
+    }
+}
