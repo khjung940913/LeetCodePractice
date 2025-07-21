b8be2347457320ab59abcc0724911d42b5c8ed98
Kristopher <nhj940913@gmail.com>
2025-07-21 23:36:23 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.3 MB (7.81%)

--- my-folder/0019-remove-nth-node-from-end-of-list/solution.java
+++ my-folder/0019-remove-nth-node-from-end-of-list/solution.java
@@ -10,20 +10,38 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode temp = head;
-        HashMap<Integer, ListNode> indexMap = new HashMap<Integer, ListNode>();
-        int s = 0;
-        while (temp != null) {
-            indexMap.put(s++, temp);
-            temp = temp.next;
+        // ListNode temp = head;
+        // HashMap<Integer, ListNode> indexMap = new HashMap<Integer, ListNode>();
+        // int s = 0;
+        // while (temp != null) {
+        //     indexMap.put(s++, temp);
+        //     temp = temp.next;
+        // }
+        // ListNode prevNode = indexMap.get(s-n-1);
+        // ListNode nextNode = indexMap.get(s-n+1);
+        // if (prevNode != null) {
+        //     prevNode.next = nextNode;
+        // } else {
+        //     return nextNode;
+        // }
+        // return head;
+
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode leader = dummy;
+        ListNode follower = dummy;
+        while (n > 0) {
+            leader = leader.next;
+            if (leader == null) return head;
+            n--;
         }
-        ListNode prevNode = indexMap.get(s-n-1);
-        ListNode nextNode = indexMap.get(s-n+1);
-        if (prevNode != null) {
-            prevNode.next = nextNode;
-        } else {
-            return nextNode;
+
+        while (leader.next != null) {
+            leader = leader.next;
+            follower = follower.next;
         }
-        return head;
+
+        follower.next = follower.next.next;
+        return dummy.next;
     }
 }
